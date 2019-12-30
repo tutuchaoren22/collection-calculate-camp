c1cd184e99b90858c9c7f32a4d28b2007b1cd271
duyuqian <15738381799@163.com>
2019-12-30 22:31:05 +0800

    [duyuqian] feat-50: each number multiply three add two,then calculate the sum

--- main/superposition_operation/collection_operation/practice_1.js
+++ main/superposition_operation/collection_operation/practice_1.js
@@ -1,9 +1,22 @@
 'use strict';
 
 function hybrid_operation(collection) {
+    var afterMultiplyThreeAddTwo = multiplyThreeAddTwo(collection);
+    var sum = sumOperation(afterMultiplyThreeAddTwo);
+    return sum;
+}
 
-  //在这里写入代码
+function multiplyThreeAddTwo(collection) {
+    var afterMultiplyThreeAddTwo = collection.map(item => (item * 3 + 2));
+    return afterMultiplyThreeAddTwo;
+
+}
+
+function sumOperation(collection) {
+    var sum = collection.reduce(function(x, y) {
+        return x + y;
+    });
+    return sum;
 }
 
 module.exports = hybrid_operation;
-
