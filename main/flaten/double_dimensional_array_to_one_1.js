47e7e36224467d7d2983a95746120d4fc04aa448
duyuqian <15738381799@163.com>
2019-12-30 09:03:36 +0800

    [duyuqian] feat-16: Turn two-dimensional array into one-dimensional array

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -2,7 +2,19 @@
 
 function double_to_one(collection) {
 
-  //在这里写入代码
+    //在这里写入代码
+    var res = flatten(collection);
+    return res
+}
+
+function flatten(arr, result = []) {
+    for (let item of arr) {
+        if (Array.isArray(item))
+            flatten(item, result)
+        else
+            result.push(item)
+    }
+    return result
 }
 
 module.exports = double_to_one;
