00f89cc970ec7f6bbc73b1298655f136a629751e
duyuqian <15738381799@163.com>
2019-12-30 08:59:26 +0800

    [duyuqian] feat-13: Select common elements in set A and set B

--- main/filter/two_collections_practice_1.js
+++ main/filter/two_collections_practice_1.js
@@ -2,7 +2,17 @@
 
 function choose_common_elements(collection_a, collection_b) {
 
-  //在这里写入代码
+    //在这里写入代码
+
+    return collection_a.filter(
+        function(item) {
+            console.log(item);
+            if (collection_b.indexOf(item) != -1) {
+                // console.log('yes'); 
+                return item;
+            }
+        }
+    );
 }
 
 module.exports = choose_common_elements;
