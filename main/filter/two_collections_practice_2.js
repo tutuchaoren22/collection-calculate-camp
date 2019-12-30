6abb0de993ec50ca0ae236a2b133b6c67f12b9e4
duyuqian <15738381799@163.com>
2019-12-30 09:00:31 +0800

    [duyuqian] feat-14: Select non-shared elements in set A and set B

--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -2,7 +2,22 @@
 
 function choose_no_common_elements(collection_a, collection_b) {
 
-  //在这里写入代码
+    //在这里写入代码
+    var d = collection_a.filter(
+        function(item) {
+            if (collection_b.indexOf(item) === -1) {
+                return item;
+            }
+        }
+    );
+    var e = collection_b.filter(
+        function(item) {
+            if (collection_a.indexOf(item) === -1) {
+                return item;
+            }
+        }
+    );
+    return d.concat(e);
 }
 
 module.exports = choose_no_common_elements;
