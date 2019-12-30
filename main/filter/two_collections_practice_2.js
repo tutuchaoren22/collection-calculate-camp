0938baffcae8bcbcc93d761decff017aafbe72c2
duyuqian <15738381799@163.com>
2019-12-30 10:35:03 +0800

    [duyuqian] refactor-14: test this card and refactor the function when find different elements

--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -3,21 +3,19 @@
 function choose_no_common_elements(collection_a, collection_b) {
 
     //在这里写入代码
-    var d = collection_a.filter(
+    var aNotIncludeB = differentElements(collection_a, collection_b)
+    var bNotIncludeA = differentElements(collection_b, collection_a)
+    return aNotIncludeB.concat(bNotIncludeA);
+}
+
+function differentElements(collection_a, collection_b) {
+    var diffElements = collection_a.filter(
         function(item) {
             if (collection_b.indexOf(item) === -1) {
                 return item;
             }
         }
     );
-    var e = collection_b.filter(
-        function(item) {
-            if (collection_a.indexOf(item) === -1) {
-                return item;
-            }
-        }
-    );
-    return d.concat(e);
+    return diffElements;
 }
-
 module.exports = choose_no_common_elements;
