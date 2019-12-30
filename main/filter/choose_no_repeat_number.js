5157039ec79de296d86b96419dbbfda26d77a37c
duyuqian <15738381799@163.com>
2019-12-30 08:56:53 +0800

    [duyuqian] feat-11: Pick unique numbers from the collection

--- main/filter/choose_no_repeat_number.js
+++ main/filter/choose_no_repeat_number.js
@@ -2,7 +2,12 @@
 
 function choose_no_repeat_number(collection) {
 
-  //在这里写入代码
+    //在这里写入代码
+    return collection.filter(
+        function(value, index, self) {
+            return self.indexOf(value) === index;
+        }
+    )
 }
 
 module.exports = choose_no_repeat_number;
