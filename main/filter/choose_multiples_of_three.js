b5df9688ff5ffef93bae8e6d39fe31607a2fcff8
duyuqian <15738381799@163.com>
2019-12-30 08:36:40 +0800

    [duyuqian] feat-10: Select a multiple of 3 from the collection use filter

--- main/filter/choose_multiples_of_three.js
+++ main/filter/choose_multiples_of_three.js
@@ -2,7 +2,8 @@
 
 function choose_multiples_of_three(collection) {
 
-  //在这里写入代码
+    //在这里写入代码
+    return collection.filter(item => (item % 3 === 0));
 }
 
 module.exports = choose_multiples_of_three;
