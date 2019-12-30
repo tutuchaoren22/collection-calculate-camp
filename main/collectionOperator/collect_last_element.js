4951be4b458b1b9cf2f51b2456c67de6ff5c945b
duyuqian <15738381799@163.com>
2019-12-30 10:19:21 +0800

    [duyuqian] fix: test the file in collectionOperator and fix the bug

--- main/collectionOperator/collect_last_element.js
+++ main/collectionOperator/collect_last_element.js
@@ -2,7 +2,7 @@
 
 function collect_last_element(collection) {
     //在这里写入代码
-    return collection.slice(-1);
+    return collection[collection.length - 1];
 }
 
 module.exports = collect_last_element;
