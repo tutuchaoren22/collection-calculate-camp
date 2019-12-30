c8c757159661787b13999aa0e208feabac3828f7
duyuqian <15738381799@163.com>
2019-12-30 08:34:20 +0800

    [duyuqian] feat-8: Get the union of two sets

--- main/collectionOperator/get_union.js
+++ main/collectionOperator/get_union.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+    //在这里写入代码
+    var res = collection_a.concat(
+        collection_b.filter(
+            function(v) {
+                return collection_a.indexOf(v) === -1
+            })
+    );
+
+    return res;
 }
 
 module.exports = get_union;
-
