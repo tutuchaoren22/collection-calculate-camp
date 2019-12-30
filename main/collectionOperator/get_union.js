379678dc43e616146f80434e7ae04cf2980670c1
duyuqian <15738381799@163.com>
2019-12-30 10:15:56 +0800

    [duyuqian] refactor-6: Refactoring tasks by splitting requirements in get_union

--- main/collectionOperator/get_union.js
+++ main/collectionOperator/get_union.js
@@ -2,14 +2,14 @@
 
 function get_union(collection_a, collection_b) {
     //在这里写入代码
-    var res = collection_a.concat(
-        collection_b.filter(
-            function(v) {
-                return collection_a.indexOf(v) === -1
-            })
-    );
+    var diffElements = findDifferentElements(collection_a, collection_b);
+    return collection_a.concat(diffElements);
+}
 
-    return res;
+function findDifferentElements(collection_a, collection_b) {
+    return collection_b.filter(function(v) {
+        return collection_a.indexOf(v) === -1
+    })
 }
 
 module.exports = get_union;
