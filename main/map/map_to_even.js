f4dacb8ef51f68f64210fdaa180b4d6e3a3cad61
duyuqian <15738381799@163.com>
2019-12-30 11:06:18 +0800

    [duyuqian] feat-18: map elements in collection to double using map function

--- main/map/map_to_even.js
+++ main/map/map_to_even.js
@@ -1,5 +1,7 @@
 'use strict';
-function map_to_even(collection){
-  return [2,4,6,8,10];
+
+function map_to_even(collection) {
+    var mapDouble = collection.map(item => item * 2);
+    return mapDouble;
 }
 module.exports = map_to_even;
