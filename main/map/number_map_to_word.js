29e0efecc561aacea32dba748692793a336244fd
duyuqian <15738381799@163.com>
2019-12-30 11:16:06 +0800

    [duyuqian] feat-22: map elements in collection to word using map function

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,6 +1,7 @@
 'use strict';
-var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+var number_map_to_word = function(collection) {
+    var mapWord = collection.map(item => String.fromCharCode(96 + item));
+    return mapWord;
 };
 
 module.exports = number_map_to_word;
