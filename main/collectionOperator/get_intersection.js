5928000f51c43d295cadb24d514bd167bcce077a
duyuqian <15738381799@163.com>
2019-12-30 08:27:58 +0800

    [duyuqian] feat-5: Find the intersection of two sets

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+    //在这里写入代码
+    return collection_b.filter(item => collection_a.includes(item))
 }
 
 module.exports = get_intersection;
