d8abd758d8a1cc5912aaf9cff4d3090c0fcd2d1c
duyuqian <15738381799@163.com>
2019-12-30 11:09:07 +0800

    [duyuqian] feat-19: map elements in collection to four multiples and add one using map function

--- main/map/map_to_four_multiples_add_one.js
+++ main/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,7 @@
 'use strict';
-var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+var map_to_four_multiples_add_one = function(collection) {
+    var mapFourMmultiplesAddOne = collection.map(item => item * 4 + 1);
+    return mapFourMmultiplesAddOne;
 };
 
 module.exports = map_to_four_multiples_add_one;
