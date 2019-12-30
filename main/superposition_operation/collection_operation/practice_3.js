21cd9741d735f5cc12af1da8374b779cb1ade505
duyuqian <15738381799@163.com>
2019-12-30 22:40:11 +0800

    [duyuqian] feat-52: each odd number multiply three add five,and then caclulate sum

--- main/superposition_operation/collection_operation/practice_3.js
+++ main/superposition_operation/collection_operation/practice_3.js
@@ -1,9 +1,26 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+    var oddNumber = findOddNumber(collection);
+    var afterMultiplyThreeAddTwo = multiplyThreeAddTwo(oddNumber);
+    var sum = sumOperation(afterMultiplyThreeAddTwo);
+    return sum;
 }
 
-module.exports = hybrid_operation_to_uneven;
+function findOddNumber(collection) {
+    var oddNumber = collection.filter(x => (x % 2 !== 0));
+    return oddNumber;
+}
 
+function multiplyThreeAddTwo(collection) {
+    var afterMultiplyThreeAddTwo = collection.map(item => (item * 3 + 5));
+    return afterMultiplyThreeAddTwo;
+}
+
+function sumOperation(collection) {
+    var sum = collection.reduce(function(x, y) {
+        return x + y;
+    });
+    return sum;
+}
+module.exports = hybrid_operation_to_uneven;
