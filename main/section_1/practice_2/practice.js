8cd706996a8e2c5901c91b224bd00d0d07813a7d
duyuqian <15738381799@163.com>
2019-12-30 17:03:27 +0800

    [duyuqian] feat-37: Find the same element between two array

--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,5 +1,6 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+    //在这里写入代码
+    return collection_b[0].filter(item => collection_a.includes(item))
 }
 
 module.exports = collect_same_elements;
