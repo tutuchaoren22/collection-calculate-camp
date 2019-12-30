bc53ea230391764ca561005748cbd2e81d4e06cc
duyuqian <15738381799@163.com>
2019-12-30 11:31:16 +0800

    [duyuqian] feat-25: find the max number in collection

--- main/reduce/collect_max_number.js
+++ main/reduce/collect_max_number.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function collect_max_number(collection) {
-  //在这里写入代码
+    //在这里写入代码
+    var maxNumber = Math.max.apply(Math, collection);
+    return maxNumber;
 }
 
 module.exports = collect_max_number;
--- main/reduce/collect_min_number.js
+++ main/reduce/collect_min_number.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function collect_min_number(collection) {
-  //在这里写入代码
+    //在这里写入代码
+    var minNumber = Math.min.apply(Math, collection);
+    return minNumber;
 }
 
 module.exports = collect_min_number;
-
