8fce1efd8baf72967311a2d6fcb07c17016dbde1
duyuqian <15738381799@163.com>
2019-12-30 12:24:52 +0800

    [duyuqian] feat-28: compute average of collection by reduce function

--- main/reduce/compute_average.js
+++ main/reduce/compute_average.js
@@ -1,8 +1,19 @@
 'use strict';
 
 function compute_average(collection) {
-  //在这里写入代码
+    // 方法一：利用for循环求和；
+    // var sum = 0;
+    // for (var i = 0; i < collection.length; i++) {
+    //     sum += collection[i];
+    // }
+    // var averageNumber = sum / collection.length;
+
+    // 方法二：利用reduce来累加求和；
+    var sum = collection.reduce(function(x, y) {
+        return x + y;
+    });
+    var averageNumber = sum / collection.length;
+    return averageNumber;
 }
 
 module.exports = compute_average;
-
