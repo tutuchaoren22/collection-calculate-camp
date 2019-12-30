2fd2401c539991312fb46985892d7516d0814d8e
duyuqian <15738381799@163.com>
2019-12-30 09:04:58 +0800

    [duyuqian] feat-17: Turn a two-dimensional array into a one-dimensional array, and output in sequence after eliminating duplicates

--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -2,7 +2,24 @@
 
 function double_to_one(collection) {
 
-  //在这里写入代码
+    //在这里写入代码
+    var res = flatten(collection);
+    return res.filter(
+        function(value, index, self) {
+            return self.indexOf(value) === index;
+        }
+    );
 }
+
+function flatten(arr, result = []) {
+    for (let item of arr) {
+        if (Array.isArray(item))
+            flatten(item, result)
+        else
+            result.push(item)
+    }
+    return result
+}
+
 
 module.exports = double_to_one;
