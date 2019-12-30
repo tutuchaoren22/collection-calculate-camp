b75fb56200d6da4f2f29f5c6d0fe4edb07bfdcaf
duyuqian <15738381799@163.com>
2019-12-30 08:26:23 +0800

    [duyuqian] feat-4: Get the array with interval 1 based on the given two numbers

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -1,8 +1,36 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+    //在这里写入代码
+    var number_c = number_b - number_a;
+    var res;
+    if (number_c > 0) {
+        res = f1(number_a, number_b);
+    } else if (number_c < 0) {
+        res = f2(number_a, number_b);
+    } else {
+        res = f3(number_a, number_b);
+    }
+    return res;
 }
 
-module.exports = get_integer_interval;
+function f1(number_a, number_b) {
+    var res = [];
+    for (var i = number_a; i <= number_b; i++) {
+        res.push(i)
+    }
+    return res;
+}
 
+function f2(number_a, number_b) {
+    var res = [];
+    for (var i = number_a; i >= number_b; i--) {
+        res.push(i)
+    }
+    return res;
+}
+
+function f3(number_a, number_b) {
+    return number_a;
+}
+module.exports = get_integer_interval;
