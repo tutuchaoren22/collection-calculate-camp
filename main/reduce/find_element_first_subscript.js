bd7df476696cec6a7c1b3f98a0ad9646756be530
duyuqian <15738381799@163.com>
2019-12-30 15:07:12 +0800

    [duyuqian] feat-32: find the element first subscript

--- main/reduce/find_element_first_subscript.js
+++ main/reduce/find_element_first_subscript.js
@@ -1,8 +1,14 @@
 'use strict';
 
-function calculate_elements_sum(collection, element) {
-  //在这里写入代码
+function find_element_first_subscript(collection, element) {
+    var index;
+    for (var i = 0; i < collection.length; i++) {
+        if (element === collection[i]) {
+            index = i;
+            break;
+        }
+    }
+    return index;
 }
 
-module.exports = calculate_elements_sum;
-
+module.exports = find_element_first_subscript;
