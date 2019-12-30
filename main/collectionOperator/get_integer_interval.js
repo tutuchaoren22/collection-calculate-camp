b4023f80695d2aaa9179a2fe5f495a983d890e5c
duyuqian <15738381799@163.com>
2019-12-30 09:52:09 +0800

    [duyuqian] refactor-4: Refactoring tasks by splitting requirements

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -2,35 +2,29 @@
 
 function get_integer_interval(number_a, number_b) {
     //在这里写入代码
-    var number_c = number_b - number_a;
-    var res;
-    if (number_c > 0) {
-        res = f1(number_a, number_b);
-    } else if (number_c < 0) {
-        res = f2(number_a, number_b);
+    if (number_a < number_b) {
+        return increaseInterval(number_a, number_b);
+    } else if (number_a > number_b) {
+        return decreaseInterval(number_a, number_b);
     } else {
-        res = f3(number_a, number_b);
+        return [number_a];
     }
-    return res;
 }
 
-function f1(number_a, number_b) {
-    var res = [];
+function increaseInterval(number_a, number_b) {
+    var resultInterval = [];
     for (var i = number_a; i <= number_b; i++) {
-        res.push(i)
+        resultInterval.push(i);
     }
-    return res;
+    return resultInterval;
 }
 
-function f2(number_a, number_b) {
-    var res = [];
+function decreaseInterval(number_a, number_b) {
+    var resultInterval = [];
     for (var i = number_a; i >= number_b; i--) {
-        res.push(i)
+        resultInterval.push(i);
     }
-    return res;
+    return resultInterval;
 }
 
-function f3(number_a, number_b) {
-    return number_a;
-}
 module.exports = get_integer_interval;
