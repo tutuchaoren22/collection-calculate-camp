27918c81413e6eb64ab3bf881a9ce06328e331cb
duyuqian <15738381799@163.com>
2019-12-30 11:24:53 +0800

    [duyuqian] feat-23: sort the collection in increasing order  use sort function

--- main/map/rank_asc.js
+++ main/map/rank_asc.js
@@ -1,6 +1,9 @@
 'use strict';
-var rank_asc = function(collection){
-  return [6,5,4,3,2];
+var rank_asc = function(collection) {
+    return collection.sort(compare);
 };
 
+function compare(val1, val2) {
+    return val2 - val1;
+};
 module.exports = rank_asc;
