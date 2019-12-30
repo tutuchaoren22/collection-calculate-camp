033a27f4ce2ac9bbae89e39300888b9f8c632795
duyuqian <15738381799@163.com>
2019-12-30 08:58:10 +0800

    [duyuqian] feat-12: Calculate the number of each number from the collection

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -2,7 +2,16 @@
 
 function grouping_count(collection) {
 
-  //在这里写入代码
+    //在这里写入代码
+    var res = {};
+    for (var i = 0; i < collection.length; i++) {
+        if (collection[i] in res) {
+            res[collection[i]]++;
+        } else {
+            res[collection[i]] = 1;
+        }
+    }
+    return res;
 }
 
 module.exports = grouping_count;
