76de4fd3b43653321cc8b2d74e4e09bf406b5a30
duyuqian <15738381799@163.com>
2019-12-30 09:02:15 +0800

    [duyuqian] feat-15: Select the number in set A that is divisible by set B

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -2,7 +2,21 @@
 
 function choose_divisible_integer(collection_a, collection_b) {
 
-  //在这里写入代码
+    //在这里写入代码
+    var res = [];
+    var ress = collection_a.filter(
+        function(item) {
+            var i;
+            for (i in collection_b) {
+                if (item % collection_b[i] === 0) {
+                    res.push(item);
+                    break;
+                }
+            }
+            return res;
+        }
+    );
+    return res;
 }
 
 module.exports = choose_divisible_integer;
