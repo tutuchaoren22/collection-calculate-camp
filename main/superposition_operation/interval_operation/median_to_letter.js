5b682551f353c921c2eae5a469450f95991291cb
duyuqian <15738381799@163.com>
2019-12-30 23:38:08 +0800

    [duyuqian] feat-54: find median of [20,53] and then change to the letter

--- main/superposition_operation/interval_operation/median_to_letter.js
+++ main/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function median_to_letter(collection) {
-
-  //在这里写入代码
+    var stra = ['0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'aa', 'ab', 'ac', 'ad', 'ae', 'af', 'ag', 'ah', 'ai', 'aj', 'ak', 'al', 'am', 'an', 'ao', 'ap',
+        'aq', 'ar', 'as', 'at', 'au', 'av', 'aw', 'ax', 'ay',
+        'az', 'ba'
+    ];
+    var len = (collection.length % 2 === 0) ? collection.length : collection.length + 1;
+    var upperMedian = Number(collection[len / 2 - 1]);
+    var lowerMedian = Number(collection[collection.length - len / 2]);
+    var median = Math.round((upperMedian + lowerMedian) / 2);
+    return stra[median];
 }
 
 module.exports = median_to_letter;
