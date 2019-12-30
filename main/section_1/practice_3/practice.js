1450227dd725c6ec1d60124a52649251a74fd0e6
duyuqian <15738381799@163.com>
2019-12-30 17:14:24 +0800

    [duyuqian] feat-39: Find the same element between array and another object

--- main/section_1/practice_3/practice.js
+++ main/section_1/practice_3/practice.js
@@ -1,5 +1,9 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+    return findSameElement(collection_a, object_b.value);
+}
+
+function findSameElement(collection_a, collection_b) {
+    return collection_a.filter(item => collection_b.includes(item));
 }
 
 module.exports = collect_same_elements;
