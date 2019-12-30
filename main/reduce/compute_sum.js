1fda0d6d4cdd73f951d48fc6c0734c75488adf77
duyuqian <15738381799@163.com>
2019-12-30 14:55:40 +0800

    [duyuqian] feat-31: compute sum of elements in array be reduce

--- main/reduce/compute_sum.js
+++ main/reduce/compute_sum.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function calculate_elements_sum(collection) {
-  //在这里写入代码
+    var sum = collection.reduce(function(x, y) {
+        return x + y;
+    });
+    return sum;
 }
 
 module.exports = calculate_elements_sum;
-
