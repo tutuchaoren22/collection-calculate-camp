790b819a208db765b7221d24fe198a01c94c1fb7
duyuqian <15738381799@163.com>
2019-12-30 11:43:28 +0800

    [duyuqian] feat-27: compare two collection after change them to string

--- main/reduce/compare_collections.js
+++ main/reduce/compare_collections.js
@@ -1,9 +1,8 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
+    //在这里写入代码
+    return (collection_a.toString() === collection_b.toString());
 }
 
 module.exports = compare_collections;
-
-
