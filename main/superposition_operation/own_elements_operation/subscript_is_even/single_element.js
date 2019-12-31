8fd4119f0cb0e74cc674eeb14a70c3a8d77d48c2
duyuqian <15738381799@163.com>
2019-12-31 09:06:41 +0800

    [duyuqian] feat-60: select the even index element

--- main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,42 @@
 'use strict';
-var single_element = function(collection){
-
+var single_element = function(collection) {
+    var oddIndexElements = findOddIndexElements(collection);
+    console.log(oddIndexElements);
+    var singleElements = deduplication(oddIndexElements);
+    return singleElements;
 };
-module.exports = single_element;
+
+function findOddIndexElements(collection) {
+    return collection.filter(function(element, index) {
+        if (index % 2 !== 0) {
+            return element;
+        }
+    });
+}
+
+function deduplication(collection) {
+    // var res = [];
+    // for (var i in collection) {
+    //     if (-1 === res.indexOf(collection[i])) {
+    //         res.push(collection[i]);
+    //         console.log(res);
+    //     } else {
+    //         var index = res.indexOf(collection[i]);
+    //         res.splice(index, 1);
+    //         console.log(res);
+    //     }
+    // }
+    var array = [];
+    for (var i = 0; i < collection.length; i++) {
+        if (array.indexOf(collection[i]) === -1) {
+            array.push(collection[i])
+        }
+    }
+    return array;
+    // return res;
+
+}
+// module.exports = single_element;
+var collection_a = [1, 2, 3, 2, 5, 6, 21, 43, 12, 5];
+var collection_b = [11, 11, 22, 11, 33, 11];
+console.log(single_element(collection_b))
