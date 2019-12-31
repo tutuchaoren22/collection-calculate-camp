e9718721b53d503e2c45a889a141aa63e39b95e4
duyuqian <15738381799@163.com>
2019-12-31 09:26:15 +0800

    [duyuqian] feat-62: Each number is added to the next number, multiplied by 3, the last number is not counted.

--- main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,5 +1,11 @@
 'use strict';
-function one_add_next_multiply_three(collection){
-  return [12,24,36,48,60,72,84,96,108,120];
+
+function one_add_next_multiply_three(collection) {
+    var newCollection = [];
+    for (var i = 0; i < collection.length - 1; i++) {
+        var newElement = 3 * (collection[i] + collection[i + 1]);
+        newCollection.push(newElement);
+    }
+    return newCollection;
 }
 module.exports = one_add_next_multiply_three;
