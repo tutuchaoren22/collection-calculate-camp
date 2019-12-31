4a6e76bffe729e5fe5c5565049da004064dc9637
duyuqian <15738381799@163.com>
2019-12-31 17:32:04 +0800

    [duyuqina] refactor-7: refactor the code of transfer number to letter over 26

--- main/collectionOperator/get_letter_interval_2.js
+++ main/collectionOperator/get_letter_interval_2.js
@@ -1,26 +1,19 @@
 'use strict';
 
-var str = ['0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'aa', 'ab', 'ac', 'ad', 'ae', 'af', 'ag', 'ah', 'ai', 'aj', 'ak', 'al', 'am', 'an', 'ao', 'ap',
-    'aq', 'ar', 'as', 'at', 'au', 'av', 'aw', 'ax', 'ay',
-    'az', 'ba'
-];
-
 function get_letter_interval_2(number_a, number_b) {
-    //在这里写入代码
     if (number_a < number_b) {
         return increaseInterval(number_a, number_b);
     } else if (number_a > number_b) {
         return decreaseInterval(number_a, number_b);
     } else {
-        return [String.fromCharCode(number_a + 96)];
+        return [transferToLetter(number_a)];
     }
 }
 
 function increaseInterval(number_a, number_b) {
     var resultInterval = [];
     for (var i = number_a; i <= number_b; i++) {
-        resultInterval.push(String.fromCharCode(i + 96));
+        resultInterval.push(transferToLetter(i));
     }
     return resultInterval;
 }
@@ -28,9 +21,17 @@ function increaseInterval(number_a, number_b) {
 function decreaseInterval(number_a, number_b) {
     var resultInterval = [];
     for (var i = number_a; i >= number_b; i--) {
-        resultInterval.push(String.fromCharCode(i + 96));
+        resultInterval.push(transferToLetter(i));
     }
     return resultInterval;
 }
 
+function transferToLetter(number) {
+    if (number <= 26) {
+        return String.fromCharCode(number + 96);
+    } else {
+        return String.fromCharCode((number - 1) / 26 + 96) + String.fromCharCode((number - 1) % 26 + 97);
+    }
+
+}
 module.exports = get_letter_interval_2;
