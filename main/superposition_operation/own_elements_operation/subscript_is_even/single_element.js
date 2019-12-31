112d1f288e1ef6b345d5b4eb7fae4175b2066822
duyuqian <15738381799@163.com>
2019-12-31 11:31:50 +0800

    [duyuqian] fix-60: select the even elements,and the unique elements.

--- main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,7 +1,6 @@
 'use strict';
 var single_element = function(collection) {
     var oddIndexElements = findOddIndexElements(collection);
-    console.log(oddIndexElements);
     var singleElements = deduplication(oddIndexElements);
     return singleElements;
 };
@@ -14,29 +13,15 @@ function findOddIndexElements(collection) {
     });
 }
 
+
 function deduplication(collection) {
-    // var res = [];
-    // for (var i in collection) {
-    //     if (-1 === res.indexOf(collection[i])) {
-    //         res.push(collection[i]);
-    //         console.log(res);
-    //     } else {
-    //         var index = res.indexOf(collection[i]);
-    //         res.splice(index, 1);
-    //         console.log(res);
-    //     }
-    // }
-    var array = [];
-    for (var i = 0; i < collection.length; i++) {
-        if (array.indexOf(collection[i]) === -1) {
-            array.push(collection[i])
+    var singleCollection = [];
+    for (var i in collection) {
+        if (collection.indexOf(collection[i]) === collection.lastIndexOf(collection[i])) {
+            singleCollection.push(collection[i]);
         }
     }
-    return array;
-    // return res;
+    return singleCollection;
 
 }
-// module.exports = single_element;
-var collection_a = [1, 2, 3, 2, 5, 6, 21, 43, 12, 5];
-var collection_b = [11, 11, 22, 11, 33, 11];
-console.log(single_element(collection_b))
+module.exports = single_element;
